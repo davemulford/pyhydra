bf30a4682ce6aadda3221ed35ae29e97a427bd10
Eric Jones <erjones@tael.usersys.redhat.com>
2020-04-28 15:22:40 -0400

    handling incoming data better, checking for lists or not

--- hydra/hydra.py
+++ hydra/hydra.py
@@ -497,11 +497,20 @@ class hydra_api:
         for k, v in kwargs.items():
             ## Fields add a much needed filter as the query will return a LOT of information without some!
             if k.lower() == "fl":
+<<<<<<< HEAD
                 query_params.update({"fl": ",".join(v)})
+=======
+                # Modifying the way the value is handed to the dictionary if handed a list or not
+                if isinstance(v, list):
+                    query_params.update({'fl': ",".join(v)})
+                else:
+                    query_params.update({'fl': v})
+>>>>>>> handling incoming data better, checking for lists or not
             # Everything else is a Query so it goes under q
             else:
                 # Modifying the way the value is handed to the dictionary if handed a list or not
                 if isinstance(v, list):
+<<<<<<< HEAD
                     if "q" in query_params.keys():
                         query_params.update(
                             {"fq": "{0}:({1})".format(k, " OR ".join(v))}
@@ -521,3 +530,13 @@ class hydra_api:
             parameters=query_params,
             headers={"Content-Type": "application/json"},
         )
+=======
+                    query_params.update({"q": "{0}:({1})".format(k, " OR ".join(v))})
+                    print(query_params)
+                else:
+                    query_params.update({"q": "{0}:({1})".format(k, v)})
+                    print(query_params)
+
+        return self.__get_api('search/cases/', parameters=query_params,
+                headers={'Content-Type': 'application/json'})
+>>>>>>> handling incoming data better, checking for lists or not
